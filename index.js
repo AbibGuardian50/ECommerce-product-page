24d804623e59997a1a1b4d81292381da7f157722
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2023-06-22 23:43:28 +0700

    fourth commit

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
-
-nextButton.addEventListener("click", (event) => {
-    const slideWidth = slide.clientWidth;
-      slidesContainer.scrollLeft += slideWidth;
-  });
-
-  nextButton.addEventListener("click", (event) => {
-    const slideWidth = slide.clientWidth;
-      slidesContainer.scrollLeft += slideWidth;
-  });
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
